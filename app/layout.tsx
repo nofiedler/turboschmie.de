bab4f319d879abb0d445c473b89b98ca9373fc17
Noah Fiedler <146343955+nofiedler@users.noreply.github.com>
2024-11-24 20:31:02 +0100

    updated contact-form

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react"
 import localFont from "next/font/local";
 import "./globals.css";
 import Footer from "@/components/ui/footer";
@@ -68,6 +69,7 @@ export default function RootLayout({
           <Footer/>
         </main>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
