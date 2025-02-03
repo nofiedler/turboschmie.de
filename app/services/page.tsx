e7c3dbbb51b86c78c4e62c6f2fb81e57683702ba
Noah Fiedler <146343955+nofiedler@users.noreply.github.com>
2025-02-03 22:31:55 +0100

    updated projects-page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -5,7 +5,7 @@ import { Services_ } from "@/components/ui/services";
 
 export default function Services() {
   return (
-    <div className="flex flex-col items-center justify-center md:h-full">
+    <div className="flex flex-col items-center justify-center md:h-full md:pt-0 pt-6">
       <h1 className="text-3xl font-bold mb-6 text-center text-white">
         Unsere Leistungen
       </h1>
