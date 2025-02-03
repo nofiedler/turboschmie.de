de27647f99d84eab0e2fcb7e31d4765eacd670e3
Noah Fiedler <146343955+nofiedler@users.noreply.github.com>
2025-02-03 20:59:41 +0100

    updated start-page

--- app/page.tsx
+++ app/page.tsx
@@ -20,14 +20,13 @@ import {
 const logo = "/logo.svg";
 
 const words = [
-  "Motoroptimierung",
-  "Steuergeräte-Abstimmung",
-  "Steuergeräte-Codierung",
-  "Fahrzeugreinigung",
+  "Softwareoptimierung",
+  "Leistungsmessung",
+  "BEDI-Reinigung",
+  "Langstreckenerprobung",
+  "Hochleistungsumbauten",
   "Sportwagen-Wartung",
-  "Sichere Autoeinlagerung",
   "Transport von Luxusfahrzeugen",
-  "Automobil-Logistik",
 ];
 
 const Skeleton = () => (
@@ -69,96 +68,88 @@ export default function Home() {
   return (
     <div className="relative w-full font-satoshi">
       <div className="md:h-screen w-full flex flex-col items-center justify-center">
-          <div className="hidden md:block">
-            <Spotlight />
-          </div>
-          <div className="flex justify-center pt-24 md:pt-60">
-            <Image
-              src={logo}
-              alt="Company Logo"
-              width={1500} // Erhöhe die Breite
-              height={500} // Erhöhe die Höhe
-              priority
-              className=""
-            />
-          </div>
-          <div className="text-xl md:text-4xl text-center mx-auto text-neutral-600 pt-10 md:pt-20">
-            Wir liefern{" "}
-            <a className="text-neutral-100 underline underline-offset-4 decoration-slate-100 decoration-1">
-              Expertenlösungen
-            </a>{" "}
-            für Hochleistungssportwagen... <br />
-            <FlipWords
-              className="text-2xl md:text-5xl font-satoshi font-black text-neutral-100 text-center pt-4"
-              words={words}
-            />
-          </div>
-          <div className="flex flex-col items-center pt-6 space-y-4">
-            <Link href="/configurator">
-              <HoverBorderGradient
-                containerClassName="rounded-full"
-                as="button"
-              >
-                <div className="flex items-center text-sm space-x-2 font-satoshi">
-                  <span>&nbsp;Tuning konfigurieren</span>
-                  <IconChevronRight className="h-4 w-4 text-white" />
-                </div>
-              </HoverBorderGradient>
-            </Link>
-          </div>
-          <div className="relative flex justify-center items-center mt-auto pb-5 text-white text-sm">
-            <ChevronsUpDownIcon />
-          </div>
+        <div className="hidden md:block">
+          <Spotlight />
         </div>
-        <div className="flex flex-col items-center md:pt-6 pt-32 space-y-4 hyphens-auto">
-          <div className="md:w-[600px] w-11/12 text-center text-white mx-auto">
-            <p className="text-3xl md:text-4xl font-light hyphens-none">
-              Unsere Philosophie
-            </p>
-            <br />
-            <br />
-            <p className="text-2xl md:text-3xl font-black">
-              &ldquo;Perfektion ist nicht erreichbar, aber wenn wir Perfektion
-              anstreben, können wir Exzellenz erreichen.&rdquo; - Vince Lombardi
-            </p>
-            <br />
-            <p className="text-lg md:text-xl font-light">
-              Bereits seit 2017 sind wir umfassend mit dem Thema der
-              Kennfeldapplikation vertraut. Durch die Faszination für
-              aufgeladene Verbrennungskraftmaschinen, haben wir es uns zur
-              Hauptaufgabe gemacht ein kompetenter, transparenter &
-              zuverlässiger Ansprechpartner im Thema Leistungssteigerung &
-              Modifikation zu sein, mit dem Schwerpunkt Deutscher
-              Premiumhersteller. Durch unsere Erfahrung der letzten 8 Jahre in
-              dieser Branche, bieten wir unsere Kompetenzen nun unteranderem
-              auch für Lamborghini, McLaren, Aston Marton, Ferrari, Porsche &
-              Bugatti an. Durch inzwischen langjährige Partnerschaften in die
-              Industrie, sind wir optimal vernetzt und können für jeden
-              Kundenwunsch das passende Ergebnis erzielen. Qualität ist bei uns
-              keine Aufgabe, sondern ein Standard.
-            </p>
-          </div>
+        <div className="flex justify-center pt-24 md:pt-60">
+          <Image
+            src={logo}
+            alt="Company Logo"
+            width={1500} // Erhöhe die Breite
+            height={500} // Erhöhe die Höhe
+            priority
+            className=""
+          />
         </div>
-        <div className="pt-24 md:pt-48">
-          <h1 className="text-2xl md:text-3xl font-bold text-center text-white hyphens-auto">
-            Impressionen <br /> aus dem vergangenen Jahr
-          </h1>
-          <InViewImagesGrid />
+        <div className="text-xl md:text-4xl text-center mx-auto text-neutral-400 pt-10 md:pt-20">
+          Wir bieten die passenden Lösungen im Bereich...{" "}
+          <FlipWords
+            className="text-2xl md:text-5xl font-satoshi font-black text-neutral-100 text-center pt-4"
+            words={words}
+          />
         </div>
-        <div className="hidden">
-          <BentoGrid className="pt-24 md:pt-52 max-w-4xl mx-auto md:auto-rows-[20rem] mb-20">
-            {items.map((item, i) => (
-              <BentoGridItem
-                key={i}
-                title={item.title}
-                description={item.description}
-                header={item.header}
-                className={`${item.className} font-satoshi`}
-                icon={item.icon}
-              />
-            ))}
-          </BentoGrid>
+        <div className="flex flex-col items-center pt-6 space-y-4">
+          <Link href="/configurator">
+            <HoverBorderGradient containerClassName="rounded-full" as="button">
+              <div className="flex items-center text-sm space-x-2 font-satoshi">
+                <span>&nbsp;Tuning konfigurieren</span>
+                <IconChevronRight className="h-4 w-4 text-white" />
+              </div>
+            </HoverBorderGradient>
+          </Link>
+        </div>
+        <div className="relative flex justify-center items-center mt-auto pb-5 text-white text-sm">
+          <ChevronsUpDownIcon />
         </div>
       </div>
+      <div className="flex flex-col items-center md:pt-6 pt-32 space-y-4 hyphens-auto">
+        <div className="md:w-[600px] w-11/12 text-center text-white mx-auto">
+          <p className="text-3xl md:text-4xl font-light hyphens-none">
+            Unsere Philosophie
+          </p>
+          <br />
+          <br />
+          <p className="text-2xl md:text-3xl font-black">
+            &ldquo;Perfektion ist nicht erreichbar, aber wenn wir Perfektion
+            anstreben, können wir Exzellenz erreichen.&rdquo; - Vince Lombardi
+          </p>
+          <br />
+          <p className="text-lg md:text-xl font-light">
+            Bereits seit 2017 sind wir umfassend mit dem Thema der
+            Kennfeldapplikation vertraut. Durch die Faszination für aufgeladene
+            Verbrennungskraftmaschinen, haben wir es uns zur Hauptaufgabe
+            gemacht ein kompetenter, transparenter & zuverlässiger
+            Ansprechpartner im Thema Leistungssteigerung & Modifikation zu sein,
+            mit dem Schwerpunkt Deutscher Premiumhersteller. Durch unsere
+            Erfahrung der letzten 8 Jahre in dieser Branche, bieten wir unsere
+            Kompetenzen nun unteranderem auch für Lamborghini, McLaren, Aston
+            Marton, Ferrari, Porsche & Bugatti an. Durch inzwischen langjährige
+            Partnerschaften in die Industrie, sind wir optimal vernetzt und
+            können für jeden Kundenwunsch das passende Ergebnis erzielen.
+            Qualität ist bei uns keine Aufgabe, sondern ein Standard.
+          </p>
+        </div>
+      </div>
+      <div className="pt-24 md:pt-48">
+        <h1 className="text-2xl md:text-3xl font-bold text-center text-white hyphens-auto">
+          Impressionen <br /> aus dem vergangenen Jahr
+        </h1>
+        <InViewImagesGrid />
+      </div>
+      <div className="hidden">
+        <BentoGrid className="pt-24 md:pt-52 max-w-4xl mx-auto md:auto-rows-[20rem] mb-20">
+          {items.map((item, i) => (
+            <BentoGridItem
+              key={i}
+              title={item.title}
+              description={item.description}
+              header={item.header}
+              className={`${item.className} font-satoshi`}
+              icon={item.icon}
+            />
+          ))}
+        </BentoGrid>
+      </div>
+    </div>
   );
 }
