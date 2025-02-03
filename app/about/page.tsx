788e97f969a0b82889cc3efd72c7da067e5c2a10
Noah Fiedler <146343955+nofiedler@users.noreply.github.com>
2025-02-03 11:38:29 +0100

    added mobile view: about-page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,19 +4,19 @@ import React from "react";
 
 export default function About() {
   return (
-    <div className="md:pt-36 flex flex-col items-center justify-center space-y-4 hyphens-auto">
+    <div className="flex flex-col items-center justify-center space-y-4 hyphens-auto md:h-full px-4">
       <div className="pt-6 text-white mx-auto">
-        <h1 className="md:w-[900px] w-10/12 text-4xl text-center font-light mb-20 mx-auto">
+        <h1 className="w-full md:w-[900px] text-3xl md:text-4xl text-center font-light mb-10 md:mb-20 mx-auto">
           &bdquo;Man kann ein Auto nicht wie ein menschliches Wesen behandeln. Ein
           Auto braucht Liebe!&ldquo; - Walter Röhrl
         </h1>
         
-        <div className="flex flex-col md:flex-row justify-center items-start space-x-12">
-          <div className="md:w-5/12 md:max-w-[600px]">
-            <h1 className="pt-6 text-3xl font-bold mb-6 text-white">
+        <div className="flex flex-col md:flex-row justify-center items-start md:space-x-12 space-y-8 md:space-y-0">
+          <div className="w-full md:w-5/12 md:max-w-[600px]">
+            <h1 className="pt-6 text-2xl md:text-3xl font-bold mb-4 md:mb-6 text-white">
               Die Entstehung der Turboschmiede
             </h1>
-            <p className="text-xl font-light">
+            <p className="text-lg md:text-xl font-light">
               Bereits in jungen Jahren & diversen kleineren &bdquo;Hobby-Garagen&ldquo; entstand
               das Verlangen einen Ort zu schaffen, wo das Thema Automobil komplett
               im Vordergrund steht. Nach 7 langen Jahren auf der Suche nach dem
@@ -29,11 +29,11 @@ export default function About() {
             </p>
           </div>
           
-          <div className="md:w-5/12 md:max-w-[600px]">
-            <h1 className="pt-6 text-3xl font-bold mb-6 text-white">
+          <div className="w-full md:w-5/12 md:max-w-[600px]">
+            <h1 className="pt-6 text-2xl md:text-3xl font-bold mb-4 md:mb-6 text-white">
               Die Vision
             </h1>
-            <p className="text-xl font-light">
+            <p className="text-lg md:text-xl font-light">
               Durch das Studium im Bereich Nachhaltige Fahrzeug- und Antriebstechnik
               und der Passion für Young- & Oldtimer, mit Schwerpunkt Deutscher
               Ingenierskunst, haben wir es uns zur Aufgabe gemacht angesichts des
