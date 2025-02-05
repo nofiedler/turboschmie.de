290d51b44067673b64d96bb4c05fc2de993e04d2
Noah Fiedler <146343955+nofiedler@users.noreply.github.com>
2025-02-05 18:37:59 +0100

    added mobile navbar

--- components/ui/mobile-navbar.tsx
+++ components/ui/mobile-navbar.tsx
@@ -82,7 +82,7 @@ export default function MobileNavbar() {
                   initial={{ opacity: 0, y: 20 }}
                   animate={{ opacity: 1, y: 0 }}
                   exit={{ opacity: 0, y: 20 }}
-                  transition={{ duration: 0.3, delay: index * 0.1 }}
+                  transition={{ duration: 0.2, delay: index * 0.1 }}
                 >
                   <Link
                     href={item.href}
